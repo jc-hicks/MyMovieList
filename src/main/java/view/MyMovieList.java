2581be17444f747e1638b336e0399743c8370c06
ElGavaOriginal <salido.r@northeastern.edu>
2025-04-14 20:11:46 -0400

    Added color to sort panel.

--- src/main/java/view/MyMovieList.java
+++ src/main/java/view/MyMovieList.java
@@ -58,6 +58,7 @@ public class MyMovieList extends JFrame {
 
         // === Sort Panel ===
         JPanel sortSubPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        sortSubPanel.setBackground(new Color(40, 40, 40));
         sortColumnCombo = new JComboBox<>(new String[]{"Year", "Title", "Director", "IMDBRating"});
         sortOrderCombo = new JComboBox<>(new String[]{"Ascending", "Descending"});
         sortButton = new JButton("Sort");
