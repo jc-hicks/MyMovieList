74218e8d3a7e577fcdf3fc3e83451d522ee8cef3
ElGavaOriginal <salido.r@northeastern.edu>
2025-04-14 21:19:33 -0400

    Added color all remaining panels.

--- src/main/java/view/RealMovieFeatures.java
+++ src/main/java/view/RealMovieFeatures.java
@@ -21,6 +21,11 @@ public class RealMovieFeatures implements IMovieFeatures {
         ((MovieModel) model).loadWatchListFromFile();
     }
 
+
+    /**(
+     * Searches record for specific title
+     * @param title
+     */
     @Override
     public void searchMovie(String title) {
         System.out.println("searchMovie called with: " + title);
@@ -33,11 +38,20 @@ public class RealMovieFeatures implements IMovieFeatures {
         }
     }
 
+    /**
+     * Adds record to personal watchlist
+     * @param title
+     */
     @Override
     public void addToWatchList(String title) {
         ((MovieModel) model).addFromRecordsToWatchList(title);
     }
 
+
+    /**
+     * Removes record from personal watchlist
+     * @param title
+     */
     @Override
     public void removeFromWatchList(String title) {
         ((MovieModel) model).removeFromWatchList(
@@ -45,16 +59,33 @@ public class RealMovieFeatures implements IMovieFeatures {
         );
     }
 
+
+    /**
+     * Pulls current watchlist
+     * @return json of movies added to list
+     */
     @Override
     public List<IMovieModel.MRecord> getWatchList() {
         return ((MovieModel) model).getWatchList();
     }
 
+
+    /**
+     * Accesses and displays record of all 200 movies
+     * @return list of 200 movie records
+     */
     @Override
     public List<IMovieModel.MRecord> getAllMovies() {
         return model.getRecords();  // Uses IMovieModel interface
     }
 
+
+    /**
+     * Sorts movie list by ascending or descending order
+     * @param column column is set by year, title, director, rating
+     * @param ascOrDesc sorts in asc/desc
+     * @return sorted list
+     */
     @Override
     public List<IMovieModel.MRecord> sortMovieList(String column, String ascOrDesc) {
         return ((MovieModel) model).sortMovieList(
@@ -64,22 +95,45 @@ public class RealMovieFeatures implements IMovieFeatures {
         );
     }
 
+
+    /**
+     * Saves watchlist as json file and calls current version
+     */
     @Override
     public void saveWatchList() {
         ((MovieModel) model).saveWatchListToFile();
     }
 
+
+    /**
+     * right click option to add personal rating to watchlist json
+     * @param title title of the movie record
+     * @param rating rating int 1-10
+     */
     @Override
     public void setMyRating(String title, String rating) {
         ((MovieModel) model).setMovieRating(title, rating);
     }
 
+
+    /**
+     * Accesses set personal set rating
+     * @param title title of the movie record
+     * @return returns int 10
+     */
     @Override
     public String getMyRating(String title) {
         IMovieModel.MRecord record = ((MovieModel) model).getRecordFromWatchList(title);
         return (record != null) ? record.imdbRating() : "N/A";
     }
 
+
+    /**
+     * Filters list of movies accessed from record or API
+     * @param field string of movie records
+     * @param criteria include title, year, director, rating
+     * @return filtered list.
+     */
     @Override
     public List<IMovieModel.MRecord> filterMovieList(String field, String criteria) {
         return ((MovieModel) model).filterWatchList(field, criteria).toList();
