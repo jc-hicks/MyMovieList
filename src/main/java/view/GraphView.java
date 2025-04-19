fcc37b29398a6da754c252ec77aeb19298c90ce1
albertusvernon <mezmo.codes@gmail.com>
2025-04-19 03:28:16 -0400

    feat: added Graph View class to display the ratings graph

---
+++ src/main/java/view/GraphView.java
@@ -0,0 +1,27 @@
+package view;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.util.List;
+
+import model.IMovieModel;
+import model.IMovieModel.MRecord;
+import javax.swing.JFrame;
+
+import controller.Controller;
+import controller.IMovieController;
+
+public class GraphView  extends JFrame {
+
+    public GraphView(IMovieController controller){
+        JFrame frame = new JFrame("Ratings Graph");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+
+    }
+
+}
