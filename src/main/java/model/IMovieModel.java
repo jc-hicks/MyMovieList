99231ee657e66c8743713ba9bfde5ac541d62097
jc-hicks <jchicks2022@gmail.com>
2025-04-21 22:03:08 -0400

    Added javadoc comments to the model from our previous implementation.

--- src/main/java/model/IMovieModel.java
+++ src/main/java/model/IMovieModel.java
@@ -16,7 +16,14 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
  */
 public interface IMovieModel {
 
+    /**
+     * The default database file path for movie records.
+     */
     String DATABASE = "data/movie.json";
+
+    /**
+     * The default database file path for the watch list.
+     */
     String WATCHLIST_DATABASE = "data/myWatchList.json";
      
     /**
@@ -26,6 +33,9 @@ public interface IMovieModel {
      */
     List<MRecord> getRecords();
 
+    /** 
+     * Get a single record by title.
+     */
     MRecord getRecord(String title);
 
     /**
@@ -71,28 +81,86 @@ public interface IMovieModel {
         }
     }
 
+    /**
+     * Retrieves a movie record from the watch list by title.
+     * @param title the title of the movie to retrieve
+     * @return the movie record, or null if not found
+     */
     MRecord getRecordFromWatchList(String title);
 
+    /**
+     * Adds a movie record to the watch list by title if it is not already there.
+     * @param title
+     */
     void addFromRecordsToWatchList(String title);
 
+    /**
+     * Removes a movie record from the watch list.
+     * @param record the movie record to remove
+     */
     void removeFromWatchList(MRecord movie);
 
+    /**
+     * Method for sorting movies based on user passed column and ordering
+     * criteria.
+     *
+     * @param movieStream
+     * @param ascOrDesc
+     * @param column
+     * @return List of movie records sorted in the appropriate order.
+     */
     List<MRecord> sortMovieList(Stream<MRecord> stream, String ascOrDesc, String column);
 
+     /**
+     * Saves the watch list to a file in JSON format.
+     *
+     */
     void saveWatchListToFile();
 
+    /**
+     * Saves the watch list to a file at the specified file path.
+     * @param filePath the path to save the file
+     */
     void saveWatchListToFilepath(String filePath, List<MRecord> watchList);
 
+    /**
+     * Sets the rating of a movie in the watch list by title.
+     * @param title the title of the movie
+     * @param rating the new rating to set
+     */
     void setMovieRating(String title, String rating);
 
+     /**
+     * Method to filter through the current list of movies based on passed
+     * criteria.
+     *
+     * @param filterType
+     * @param filterValue
+     * @return Stream of movie records
+     */
     Stream<MRecord> filterWatchList(String field, String criteria);
 
+    /**
+     * Removes a movie record from the watch list by title.
+     * @param title the title of the movie to remove
+     */
     List<MRecord> getWatchList();
 
+    /**
+     * Loads the watch list from a file at the specified file path.
+     * @param filePath
+     */
     void loadWatchListFromFile(String filePath);
 
+    /**
+     * Loads the watch list from the default file.
+     */
     void loadWatchListFromFile();
     
+    /**
+     * Movie record class representing a movie with various attributes.
+     * This class is used for JSON serialization and deserialization.
+     */
     @JsonIgnoreProperties(ignoreUnknown = true)
     @JacksonXmlRootElement(localName = "movie")
     @JsonPropertyOrder({"Title", "Year", "Director", "Actors", "Plot", "Poster",
